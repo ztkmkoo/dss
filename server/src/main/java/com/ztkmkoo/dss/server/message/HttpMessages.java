035d028e441fb6b339188efcb410e26a19188072
ztkmkoo <ztkmkoo@gmail.com>
2020-02-09 18:18:07 +0900

    [#17] actor module
    - add http message for actor model
    - add akka dependency

---
+++ server/src/main/java/com/ztkmkoo/dss/server/message/HttpMessages.java
@@ -0,0 +1,60 @@
+package com.ztkmkoo.dss.server.message;
+
+import akka.actor.typed.ActorRef;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Project: dss
+ * Created by: @ztkmkoo(ztkmkoo@gmail.com)
+ * Date: 20. 2. 9. 오후 6:07
+ */
+public class HttpMessages {
+
+    private HttpMessages() {}
+
+    public static class Request implements Serializable {
+
+        private static final LinkedList<ActorRef<Request>> emptyQueue = new LinkedList<>();
+        private static final long serialVersionUID = -5018351808316127595L;
+
+        @Getter
+        private final String path;
+        @Getter
+        private final String content;
+        @Getter
+        private final ActorRef<HttpMessages.Response> httpResponseHandlerActor;
+        @Getter
+        private final Queue<ActorRef<Request>> filterQueue;
+        @Getter
+        private final ActorRef<HttpMessages.Request> businessActor;
+
+        public Request(
+                String path,
+                String content,
+                ActorRef<HttpMessages.Response> httpResponseHandlerActor,
+                List<ActorRef<Request>> filterQueue,
+                ActorRef<HttpMessages.Request> businessActor) {
+            this.path = path;
+            this.content = content;
+            this.httpResponseHandlerActor = httpResponseHandlerActor;
+            this.filterQueue = new LinkedList<>(filterQueue);
+            this.businessActor = businessActor;
+        }
+
+        public Request(
+                HttpMessages.Request request,
+                ActorRef<HttpMessages.Response> httpResponseHandlerActor,
+                ActorRef<HttpMessages.Request> businessActor
+        ) {
+            this(request.path, request.content, httpResponseHandlerActor, emptyQueue, businessActor);
+        }
+    }
+
+    public static class Response implements Serializable {
+    }
+}
