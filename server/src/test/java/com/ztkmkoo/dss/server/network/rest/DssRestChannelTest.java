8ad308456bd8b67b505231fb7e75e9449024a476
ztkmkoo <ztkmkoo@gmail.com>
2020-02-19 01:18:44 +0900

    [#22] improve DssRestChannelTest
    - add client socket read timeout test case

--- server/src/test/java/com/ztkmkoo/dss/server/network/rest/DssRestChannelTest.java
+++ server/src/test/java/com/ztkmkoo/dss/server/network/rest/DssRestChannelTest.java
@@ -10,9 +10,13 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 import static org.junit.Assert.assertTrue;
 
@@ -38,19 +42,7 @@ public class DssRestChannelTest {
         final NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
-            final DssRestChannel dssRestChannel = new DssRestChannel();
-
-            final ServerBootstrap b = new ServerBootstrap()
-                    .group(bossGroup, workerGroup);
-
-            final DssRestChannelProperty property = DssRestChannelProperty
-                    .builder(dssChannelInitializer)
-                    .port(8181)
-                    .build();
-
-            final Channel channel = dssRestChannel.bind(b, property);
-            channel.closeFuture();
-
+            bindServer(bossGroup, workerGroup, 8181);
             assertTrue(serverPortIsOpen(8181));
         } finally {
             workerGroup.shutdownGracefully();
@@ -58,10 +50,63 @@ public class DssRestChannelTest {
         }
     }
 
+    @Test(expected = SocketTimeoutException.class)
+    public void bindAndSendTimeout() throws InterruptedException, IOException {
+
+        final NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        final NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            bindServer(bossGroup, workerGroup, 8181);
+            sendMessageToServer(8181);
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    private void bindServer(
+            NioEventLoopGroup bossGroup,
+            NioEventLoopGroup workerGroup,
+            int port
+    ) throws InterruptedException {
+
+        final DssRestChannel dssRestChannel = new DssRestChannel();
+
+        final ServerBootstrap b = new ServerBootstrap()
+                .group(bossGroup, workerGroup);
+
+        final DssRestChannelProperty property = DssRestChannelProperty
+                .builder(dssChannelInitializer)
+                .port(port)
+                .build();
+
+        final Channel channel = dssRestChannel.bind(b, property);
+        channel.closeFuture();
+    }
+
     private boolean serverPortIsOpen(int port) throws IOException {
 
         final Socket socket = new Socket();
         socket.connect(new InetSocketAddress("127.0.0.1", port));
         return socket.isConnected();
+    }
+
+    private void sendMessageToServer(int port) throws IOException {
+
+        final Socket socket = new Socket();
+        socket.setSoTimeout(3000);
+        socket.connect(new InetSocketAddress("127.0.0.1", port));
+
+        if (socket.isConnected()) {
+            final DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            outputStream.writeByte(1);
+            outputStream.flush();
+
+            final String response = reader.readLine();
+            System.err.println(response);
+        }
     }
 }
