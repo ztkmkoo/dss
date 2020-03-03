e3ab4e2abbc242b2017743951428d502413796e1
ztkmkoo <ztkmkoo@gmail.com>
2020-03-04 00:55:52 +0900

    [#25] remove old version module

--- server/src/main/java/com/ztkmkoo/dss/server/network/core/entity/DssChannel.java
+++
@@ -1,9 +0,0 @@
-package com.ztkmkoo.dss.server.network.core.entity;
-
-/**
- * Project: dss
- * Created by: @ztkmkoo(ztkmkoo@gmail.com)
- * Date: 20. 2. 17. 오후 10:00
- */
-public interface DssChannel {
-}
