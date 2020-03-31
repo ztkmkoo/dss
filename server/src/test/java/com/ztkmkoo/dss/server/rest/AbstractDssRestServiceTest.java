096050441d9db04ad00c64cd2c7f1ac8703737e4
ztkmkoo <ztkmkoo@gmail.com>
2020-04-01 04:57:49 +0900

    [#29] fix tests

--- server/src/test/java/com/ztkmkoo/dss/server/rest/AbstractDssRestServiceTest.java
+++ server/src/test/java/com/ztkmkoo/dss/server/rest/AbstractDssRestServiceTest.java
@@ -1,7 +1,9 @@
 package com.ztkmkoo.dss.server.rest;
 
+import com.ztkmkoo.dss.core.actor.rest.entity.DssRestContentInfo;
 import com.ztkmkoo.dss.core.actor.rest.entity.DssRestServiceRequest;
 import com.ztkmkoo.dss.core.actor.rest.entity.DssRestServiceResponse;
+import com.ztkmkoo.dss.core.message.rest.DssRestServiceActorCommandRequest;
 import com.ztkmkoo.dss.core.network.rest.enumeration.DssRestMethodType;
 import com.ztkmkoo.dss.server.rest.service.AbstractDssRestService;
 import org.junit.Test;
@@ -21,7 +23,22 @@ public class AbstractDssRestServiceTest {
         final AbstractDssRestService dssRestService = new AbstractDssRestService("test", "/hi", DssRestMethodType.POST) {
 
             @Override
+            public DssRestContentInfo getConsume() {
+                return null;
+            }
+
+            @Override
+            public DssRestContentInfo getProduce() {
+                return null;
+            }
+
+            @Override
             public DssRestServiceResponse handling(DssRestServiceRequest request) {
+                return null;
+            }
+
+            @Override
+            public DssRestServiceRequest convertRequest(DssRestServiceActorCommandRequest commandRequest) {
                 return null;
             }
         };
--- server/src/test/java/com/ztkmkoo/dss/server/rest/DssRestServerTest.java
+++ server/src/test/java/com/ztkmkoo/dss/server/rest/DssRestServerTest.java
@@ -1,8 +1,10 @@
 package com.ztkmkoo.dss.server.rest;
 
+import com.ztkmkoo.dss.core.actor.rest.entity.DssRestContentInfo;
+import com.ztkmkoo.dss.core.actor.rest.entity.DssRestServiceRequest;
+import com.ztkmkoo.dss.core.actor.rest.entity.DssRestServiceResponse;
+import com.ztkmkoo.dss.core.message.rest.DssRestServiceActorCommandRequest;
 import com.ztkmkoo.dss.core.network.rest.enumeration.DssRestMethodType;
-import com.ztkmkoo.dss.server.rest.entity.DssRestServiceRequestWrapper;
-import com.ztkmkoo.dss.server.rest.entity.DssRestServiceResponseWrapper;
 import com.ztkmkoo.dss.server.rest.service.AbstractDssRestService;
 import org.junit.Test;
 
@@ -20,8 +22,24 @@ public class DssRestServerTest {
 
         final DssRestServer dssRestServer = new DssRestServer("127.0.0.1", 8181);
         dssRestServer.addDssRestService(new AbstractDssRestService("test", "/hi", DssRestMethodType.GET) {
+
             @Override
-            public DssRestServiceResponseWrapper handling(DssRestServiceRequestWrapper request) {
+            public DssRestContentInfo getConsume() {
+                return null;
+            }
+
+            @Override
+            public DssRestContentInfo getProduce() {
+                return null;
+            }
+
+            @Override
+            public DssRestServiceResponse handling(DssRestServiceRequest request) {
+                return null;
+            }
+
+            @Override
+            public DssRestServiceRequest convertRequest(DssRestServiceActorCommandRequest commandRequest) {
                 return null;
             }
         });
