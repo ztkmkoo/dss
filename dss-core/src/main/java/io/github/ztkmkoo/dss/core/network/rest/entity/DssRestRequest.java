c11237e6aad47e9129e9ddffc1341eee86bffa1e
YeEunLee <37532204+RulLu16@users.noreply.github.com>
2020-08-15 14:50:54 +0900

    [#97] bug fix: can't get response when using charset error. (#101)

--- dss-core/src/main/java/io/github/ztkmkoo/dss/core/network/rest/entity/DssRestRequest.java
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/network/rest/entity/DssRestRequest.java
@@ -18,13 +18,15 @@ public class DssRestRequest implements Serializable {
     private static final long serialVersionUID = 8456589353995730809L;
     private final DssRestMethodType methodType;
     private final DssRestContentType contentType;
+    private final String charset;
     private final String uri;
     private final String content;
 
     @Builder
-    protected DssRestRequest(DssRestMethodType methodType, String uri, DssRestContentType contentType, String content) {
+    protected DssRestRequest(DssRestMethodType methodType, String uri, DssRestContentType contentType, String charset, String content) {
         this.methodType = methodType;
         this.contentType = contentType;
+        this.charset = charset;
         this.uri = uri;
         this.content = content;
     }
