d89c348a671e7be56bf60ad6e38d681dcc0de293
yeonnseok <ljinsk3@gmail.com>
2020-08-19 22:14:38 +0900

    Implement Handler to listen TCP

---
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/network/tcp/handler/DssTcpChannelInitializer.java
@@ -0,0 +1,29 @@
+package io.github.ztkmkoo.dss.core.network.tcp.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class DssTcpChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final Logger logger = LoggerFactory.getLogger(DssTcpChannelInitializer.class);
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        logger.info("Try to initChannel");
+
+        final ChannelPipeline p = ch.pipeline();
+
+        p.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        p.addLast(new StringDecoder());
+        p.addLast(new StringEncoder());
+        p.addLast(new DssTcpHandler());
+    }
+}
---
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/network/tcp/handler/DssTcpHandler.java
@@ -0,0 +1,34 @@
+package io.github.ztkmkoo.dss.core.network.tcp.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class DssTcpHandler extends SimpleChannelInboundHandler<String> {
+
+    private final Logger logger = LoggerFactory.getLogger(DssTcpHandler.class);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String message) throws Exception {
+        logger.info("Channel Reading Start");
+
+        ChannelFuture future = channelHandlerContext.write(message + "\n");
+
+        future.addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext channelHandlerContext) {
+        channelHandlerContext.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable cause) {
+        cause.printStackTrace();
+        channelHandlerContext.close();
+    }
+}
