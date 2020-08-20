afc9762ca98203a43b5a38b7b18644385e21b293
yeonnseok <ljinsk3@gmail.com>
2020-08-21 03:16:15 +0900

    add DssTcpSslChannelInitializerTest

---
+++ dss-core/src/test/java/io/github/ztkmkoo/dss/core/network/tcp/handler/DssTcpSslChannelInitializerTest.java
@@ -0,0 +1,53 @@
+package io.github.ztkmkoo.dss.core.network.tcp.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.ssl.SslContext;
+
+class DssTcpSslChannelInitializerTest {
+
+    @Mock
+    private SocketChannel socketChannel;
+
+    @Mock
+    private ChannelPipeline channelPipeline;
+
+    @Mock
+    private SslContext sslCtx;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void initChannel() throws Exception {
+        Mockito
+            .when(socketChannel.pipeline())
+            .thenReturn(channelPipeline);
+
+        Mockito
+            .when(channelPipeline.addLast(any()))
+            .thenReturn(channelPipeline);
+
+        Mockito
+            .when(channelPipeline.last())
+            .thenReturn(new DssTcpHandler());
+
+        final DssTcpSslChannelInitializer dssTcpSslChannelInitializer = Mockito.mock(DssTcpSslChannelInitializer.class);
+
+        dssTcpSslChannelInitializer.initChannel(socketChannel);
+
+        final ChannelPipeline p = socketChannel.pipeline();
+        assertEquals(DssTcpHandler.class, p.last().getClass());
+    }
+}
