6af0b7444832edd8e7a16852122ffef7f93e4009
ztkmk <ztkmkoo@gmail.com>
2020-09-03 22:49:23 +0900

    [#118] redesign actor
    - revert DssChannelProperty.java to c7d3c2d2f907a7dffc151b5cfa8c6ec0b07e4d7f

--- dss-core/src/main/java/io/github/ztkmkoo/dss/core/network/DssChannelProperty.java
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/network/DssChannelProperty.java
@@ -1,10 +1,8 @@
 package io.github.ztkmkoo.dss.core.network;
 
-import java.io.Serializable;
-
 /**
  * @author Kebron ztkmkoo@gmail.com
  * @create 2020-08-01 21:32
  */
-public interface DssChannelProperty extends Serializable {
+public interface DssChannelProperty {
 }
