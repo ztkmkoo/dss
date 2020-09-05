fe144ce8b712b10ca67ea5e574d91ff1de877319
ztkmk <ztkmkoo@gmail.com>
2020-09-06 04:36:35 +0900

    [#118] redesign actor
    - add DssExceptionActor and its command interface

---
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/message/DssExceptionCommand.java
@@ -0,0 +1,8 @@
+package io.github.ztkmkoo.dss.core.message;
+
+/**
+ * @author Kebron ztkmkoo@gmail.com
+ * @create 2020-09-06 04:35
+ */
+public interface DssExceptionCommand extends DssCommand {
+}
