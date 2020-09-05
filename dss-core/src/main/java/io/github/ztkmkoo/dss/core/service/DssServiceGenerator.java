00067d56da2e165000a24e30480ff89678b009e6
ztkmk <ztkmkoo@gmail.com>
2020-09-06 02:28:21 +0900

    [#118] redesign actor
    - add ObjectUtils.java
    - add service interface

---
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/service/DssServiceGenerator.java
@@ -0,0 +1,13 @@
+package io.github.ztkmkoo.dss.core.service;
+
+/**
+ * @author Kebron ztkmkoo@gmail.com
+ * @create 2020-09-06 01:22
+ */
+public interface DssServiceGenerator {
+
+    /**
+     * DssService create
+     */
+    DssService create();
+}
