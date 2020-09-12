7e74b71f7caf2c378c1be34996b903029e633cd6
JuHyun419 <zzang9ha@naver.com>
2020-09-12 15:25:44 +0900

    remove unused method

--- dss-core/src/main/java/io/github/ztkmkoo/dss/core/message/blocking/jdbc/DssSelectCommand.java
+++ dss-core/src/main/java/io/github/ztkmkoo/dss/core/message/blocking/jdbc/DssSelectCommand.java
@@ -4,13 +4,10 @@ import io.github.ztkmkoo.dss.core.message.DssCommand;
 
 import java.util.Map;
 
-public interface DssSelectCommand<T> extends DssCommand {
+public interface DssSelectCommand extends DssCommand {
 	
-//	String getNamespace();
-//	String getQueryId();
-//	T getEntity();
-
 	String getTableName();
 	String[] getColumns();
 	Map<String, String> getParam();
+	
 }
