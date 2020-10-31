6a5f748fb8a1789d29681d43e21353c63fa8652b
ztkmk <ztkmkoo@gmail.com>
2020-11-01 03:49:34 +0900

    [#142] implement new network actor
    - add dss server bootstrap interface and its rest implement

---
+++ dss-server/src/main/java/io/github/ztkmkoo/dss/server/rest/DssRestServerBootstrap.java
@@ -0,0 +1,15 @@
+package io.github.ztkmkoo.dss.server.rest;
+
+import io.github.ztkmkoo.dss.server.DssServerBootstrap;
+
+/**
+ * @author Kebron ztkmkoo@gmail.com
+ * @create 2020-10-26 23:06
+ */
+public class DssRestServerBootstrap implements DssServerBootstrap {
+
+    @Override
+    public void start() throws InterruptedException {
+        // do sth
+    }
+}
