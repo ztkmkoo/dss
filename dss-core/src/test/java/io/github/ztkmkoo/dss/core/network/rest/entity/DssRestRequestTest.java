1f6e280466fd51a70a8977b1807ef22e59c4dd0e
ztkmk <ztkmkoo@gmail.com>
2020-11-15 23:36:53 +0900

    [#142] implement new network actor
    - add test for DssRestRequest.java
    - add test for DssRestContentType.java
    - delete unused classes

--- dss-core/src/test/java/io/github/ztkmkoo/dss/core/network/rest/entity/DssRestRequestTest.java
+++ dss-core/src/test/java/io/github/ztkmkoo/dss/core/network/rest/entity/DssRestRequestTest.java
@@ -1,10 +1,11 @@
 package io.github.ztkmkoo.dss.core.network.rest.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import io.github.ztkmkoo.dss.core.network.rest.enumeration.DssRestContentType;
+import io.github.ztkmkoo.dss.core.network.rest.enumeration.DssRestMethodType;
 import org.junit.jupiter.api.Test;
 
-import io.github.ztkmkoo.dss.core.network.rest.enumeration.DssRestMethodType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Project: dss
@@ -21,11 +22,15 @@ class DssRestRequestTest {
                 .methodType(DssRestMethodType.GET)
                 .uri("/hi")
                 .content("Hello")
+                .charset("UTF-8")
+                .contentType(DssRestContentType.APPLICATION_JSON)
                 .build();
 
         assertNotNull(request);
         assertEquals(DssRestMethodType.GET, request.getMethodType());
         assertEquals("/hi", request.getUri());
         assertEquals("Hello", request.getContent());
+        assertEquals(DssRestContentType.APPLICATION_JSON, request.getContentType());
+        assertEquals("UTF-8", request.getCharset());
     }
 }
--- dss-core/src/test/java/io/github/ztkmkoo/dss/core/network/rest/enumeration/DssRestContentTypeTest.java
+++ dss-core/src/test/java/io/github/ztkmkoo/dss/core/network/rest/enumeration/DssRestContentTypeTest.java
@@ -1,8 +1,9 @@
 package io.github.ztkmkoo.dss.core.network.rest.enumeration;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Project: dss
@@ -13,8 +14,19 @@ class DssRestContentTypeTest {
 
     @Test
     void fromText() {
-
         final DssRestContentType type = DssRestContentType.fromText("application/json");
         assertEquals(DssRestContentType.APPLICATION_JSON, type);
+    }
+
+    @Test
+    void testNotExist() {
+        final DssRestContentType type = DssRestContentType.fromText("application/jsonT");
+        assertNull(type);
+    }
+
+    @Test
+    void testNotExist2() {
+        final DssRestContentType type = DssRestContentType.fromText(null);
+        assertNull(type);
     }
 }
